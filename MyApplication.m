975da1d68fe9538f08e455a381d6a97a38da3939
Ali Asadullah <ali.asadullah@swenggco-software.com>
2015-05-11 18:28:14 +0500

    Send Event Method for all app button taps

---
+++ MyApplication.m
@@ -0,0 +1,78 @@
+//
+//  MyApplication.m
+//  FitnessElements
+//
+//  Created by Ali Asadullah on 11/05/2015.
+//  Copyright (c) 2015 SwenggCo. All rights reserved.
+//
+
+#import "MyApplication.h"
+#import "SignInViewController.h"
+#import "PopupViewController.h"
+
+#import "AppDelegate.h"
+
+@implementation MyApplication
+-(void)sendEvent:(UIEvent *)event
+{
+    
+    NSSet *set= event.allTouches;
+    NSArray *array=[set allObjects];
+    UITouch *touchEvent= [array lastObject];
+    UIView *view=[touchEvent view];
+    if([view isKindOfClass:[UIButton class]])
+    {
+        UIButton *button = (UIButton *)view;
+
+        for (id target in button.allTargets)
+        {
+            if (touchEvent.phase== UITouchPhaseEnded) {
+                
+                            PopupViewController *alertView = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]]instantiateViewControllerWithIdentifier:@"PopupViewController"];
+                if([target isKindOfClass:[ABC class]] || [target isKindOfClass:[XYZ class]])
+                {
+                    if([userNOTSignedIn]){
+                            alertView.modalPresentationStyle = UIModalPresentationOverCurrentContext;
+                            [target presentViewController:alertView animated:YES completion:NULL];
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+            
+            
+//            [[[UIAlertView alloc]initWithTitle:@"Alert" message:@"mmm" delegate:[AppDelegate sharedDalagate] cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil] show];
+//
+//            return;
+//            UIViewController *viewController = (UIViewController *)target;
+//            
+//            PopupViewController *signinViewController = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]]instantiateViewControllerWithIdentifier:@"PopupViewController"];
+//            signinViewController.modalPresentationStyle = UIModalPresentationOverCurrentContext;
+//            [viewController presentViewController:signinViewController animated:YES completion:NULL];
+//            return;
+//            
+//            if(![viewController isKindOfClass:[SignInViewController class]]){
+//                PopupViewController *signinViewController = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]]instantiateViewControllerWithIdentifier:@"PopupViewController"];
+//                signinViewController.modalPresentationStyle = UIModalPresentationOverCurrentContext;
+//                [viewController presentViewController:signinViewController animated:YES completion:NULL];
+//                return;
+//                for (NSString *action in actions) {
+//                    
+//                    [newButton addTarget:target action:NSSelectorFromString(action) forControlEvents:UIControlEventTouchUpInside];
+//                }
+////            }
+//        }
+//        //        NSArray *actions = [button actionsForTarget:nil
+//        //                                    forControlEvent:UIControlEventTouchUpInside];
+//        //        id action=[button actionForLayer:[itsSuperView layer] forKey:@"touchUpInside"];
+//    }
+////    [super sendEvent:event];
+//    
+//    
+//}
+
+
+@end
